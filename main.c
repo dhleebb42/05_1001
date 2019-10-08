18e61d5ddec916143a9dd4ed5fac628f7e6e082e
dhleebb42 <55428726+dhleebb42@users.noreply.github.com>
2019-10-08 16:01:43 +0900

    06_1008(2)

--- main.c
+++ main.c
@@ -5,13 +5,19 @@
 
 int main(int argc, char *argv[]) 
 {
-	int i=0, meter;
+	int num=0;
+	char c;
 	
-	while (i<3)
-	{	
-		meter = i*1609;
-		printf("%d 마일은 %d 미터입니다.\n", i, meter);
-		i++;
+	printf("input a string : ");
+	
+	while((c=getchar())!='\n')
+	{
+		if(c<='9' && c >='0')
+		{
+			num++;
+		}
 	}
+	
+	printf("%i\n", num);
 	return 0;
 }
