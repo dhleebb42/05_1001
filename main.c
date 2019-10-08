4242c37d37978bad8866282154d349a0e48cf523
dhleebb42 <55428726+dhleebb42@users.noreply.github.com>
2019-10-08 16:19:02 +0900

    06_1008(3)

--- main.c
+++ main.c
@@ -5,19 +5,26 @@
 
 int main(int argc, char *argv[]) 
 {
-	int num=0;
-	char c;
+	int answer = 59;
+	int input;
+	int cnt=0;
 	
-	printf("input a string : ");
-	
-	while((c=getchar())!='\n')
+	do
 	{
-		if(c<='9' && c >='0')
+		printf("input a number :");
+		scanf("%d", &input);
+		if(input < answer)
 		{
-			num++;
+			printf("low!\n");
 		}
+		else if (input > answer)
+		{
+			printf("hign!\n");
+		}
+		cnt++;
 	}
-	
-	printf("%i\n", num);
+	while(input != answer);
+
+	printf("Congratulation! trials : %i\n", cnt);
 	return 0;
 }
